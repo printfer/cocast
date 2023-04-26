ced9d5329e4bb70b910451939fc772a935980fbe
printfer <16911871+printfer@users.noreply.github.com>
2023-04-26 00:32:36 -0300

    Split useRoomStatus hook into smaller, more manageable pieces for enhanced readability and maintainability

--- src/hooks/useRoomStatus.ts
+++ src/hooks/useRoomStatus.ts
@@ -29,20 +29,19 @@ export default function useRoomStatus(): [
   RoomStatus,
   (status: RoomStatus) => void
 ] {
+  // TODO: another default media url option: https://youtu.be/JOhiWY7XmoY
+  const defaultMediaURL =
+    "https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4";
+
   // Local media status
-  // TODO: change the init value later
   const [mediaPlayList, setMediaPlayList] = useState<string[]>([
-    "https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4",
+    defaultMediaURL,
   ]);
-  //const [mediaPlayList, setMediaPlayList] = useState<string[]>(["https://youtu.be/JOhiWY7XmoY"]);
-  // TODO: change the init value later
-  const [mediaSource, setMediaSource] = useState<string>(
-    "https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4"
-  );
-  //const [mediaSource, setMediaSource] = useState<string>("https://youtu.be/JOhiWY7XmoY");
+  const [mediaSource, setMediaSource] = useState<string>(defaultMediaURL);
   const [mediaAction, setMediaAction] = useState<MediaCode>(MediaCode.Pause);
   const [mediaTime, setMediaTime] = useState<number>(0);
   const [mediaSpeed, setMediaSpeed] = useState<number>(1);
+
   // Remote status
   const [isRemoteMediaControl, setIsRemoteMediaControl] =
     useState<boolean>(false);
@@ -51,6 +50,7 @@ export default function useRoomStatus(): [
   // Local peer status: peerId, peerInstance
   const [peerId, setPeerId] = useState<string | null>(null);
   const peerInstance = useRef<Peer | null>(null);
+
   // Remote peer status: remotePeerList, totalPeerNumber
   const peerConnections = useRef(new Map<string, DataConnection>());
   const [peerConnectionsState, setPeerConnectionsState] = useState(
@@ -69,7 +69,7 @@ export default function useRoomStatus(): [
     setPeerConnectionsState(new Map(peerConnections.current));
   };
 
-  // Init connection
+  // Initialize connection
   useEffect(() => {
     const peer = new Peer();
 
@@ -101,7 +101,7 @@ export default function useRoomStatus(): [
     }
   }, [peerId]);
 
-  // Connection Control
+  // Connection control codes
   const enum ControlCode {
     SyncPeerInit,
     SyncPeer,
@@ -139,98 +139,15 @@ export default function useRoomStatus(): [
 
       // Receive messages
       conn.on("data", (data) => {
-        const peerData = data as PeerData;
-        switch (peerData.controlCode) {
-          // Init sync
-          case ControlCode.SyncPeerInit: {
-            console.debug(
-              "[room] sync peer init request from:",
-              peerData.controlMessage
-            );
-            if (peerConnections.current.size === 1) {
-              break;
-            }
-            // Send this new connected peer to all other peers
-            for (const [
-              peerKey,
-              peerConn,
-            ] of peerConnections.current.entries()) {
-              if (peerKey !== conn.peer) {
-                peerConn.send({
-                  controlCode: ControlCode.SyncPeer,
-                  controlMessage: [conn.peer],
-                } as ConnectionData);
-              }
-            }
-            // Send all other peers to this new connected peer
-            conn.send({
-              controlCode: ControlCode.SyncPeer,
-              controlMessage: [...peerConnections.current.keys()].filter(
-                (peerKey) => peerKey !== conn.peer
-              ),
-            } as ConnectionData);
-            break;
-          }
-          // Sync peer
-          case ControlCode.SyncPeer: {
-            console.debug("[room] sync peer for:", peerData.controlMessage);
-            // Connect to received peers if not inside current peerConnections
-            for (const peerKey of (peerData as ConnectionData).controlMessage) {
-              if (!peerConnections.current.has(peerKey)) {
-                const conn = peerInstance.current?.connect(
-                  peerKey
-                ) as DataConnection;
-                connectionControl(conn);
-              }
-            }
-            break;
-          }
-          // Fetch Media status
-          case ControlCode.FetchMediaStatus: {
-            console.debug("[room] fetch media status for:", conn.peer);
-            setIsRemoteFetch(conn.peer);
-            break;
-          }
-          // Media status update
-          case ControlCode.UpdateMediaStatus: {
-            console.debug(
-              "[room] update local media status to:",
-              peerData.controlMessage
-            );
-            const {
-              mediaSource,
-              mediaAction,
-              mediaTime,
-              mediaSpeed,
-              mediaPlayList,
-            } = (peerData as MediaData).controlMessage;
-            // Local media status
-            setMediaPlayList(mediaPlayList);
-            setMediaSource(mediaSource);
-            setMediaAction(mediaAction);
-            setMediaTime(mediaTime);
-            setMediaSpeed(mediaSpeed);
-            // Remote status
-            setIsRemoteMediaControl(true);
-            break;
-          }
-          default:
-            console.warn("[room] Unknown data received", data);
-        }
+        handleReceivedData(data as PeerData, conn);
       });
 
       // Send messages
       if (sharedId) {
         // Init peer connection
-        conn.send({
-          controlCode: ControlCode.SyncPeerInit,
-          controlMessage: [conn.peer],
-        } as ConnectionData);
+        sendSyncPeerInit(conn);
         // Fetch all media status from peer
-        conn.send({
-          controlCode: ControlCode.FetchMediaStatus,
-          controlMessage: [conn.peer],
-        } as ConnectionData);
+        sendFetchMediaStatus(conn);
       }
     });
 
@@ -239,6 +156,115 @@ export default function useRoomStatus(): [
       // Remove closed peer connection from peerConnections
       peerConnectionsDelete(conn.peer);
     });
+  };
+
+  const handleReceivedData = (data: PeerData, conn: DataConnection) => {
+    const peerData = data as PeerData;
+    switch (peerData.controlCode) {
+      case ControlCode.SyncPeerInit:
+        handleSyncPeerInit(peerData, conn);
+        break;
+      case ControlCode.SyncPeer:
+        handleSyncPeer(peerData);
+        break;
+      case ControlCode.FetchMediaStatus:
+        handleFetchMediaStatus(conn);
+        break;
+      case ControlCode.UpdateMediaStatus:
+        handleUpdateMediaStatus(peerData);
+        break;
+      default:
+        console.warn("[room] Unknown data received", data);
+    }
+  };
+
+  const handleSyncPeerInit = (peerData: PeerData, conn: DataConnection) => {
+    console.debug(
+      "[room] sync peer init request from:",
+      peerData.controlMessage
+    );
+    if (peerConnections.current.size === 1) {
+      return;
+    }
+    // Send this new connected peer to all other peers
+    for (const [peerKey, peerConn] of peerConnections.current.entries()) {
+      if (peerKey !== conn.peer) {
+        sendSyncPeer(peerConn, [conn.peer]);
+      }
+    }
+    // Send all other peers to this new connected peer
+    const otherPeers = [...peerConnections.current.keys()].filter(
+      (peerKey) => peerKey !== conn.peer
+    );
+    sendSyncPeer(conn, otherPeers);
+  };
+
+  const handleSyncPeer = (peerData: PeerData) => {
+    console.debug("[room] sync peer for:", peerData.controlMessage);
+    // Connect to received peers if not inside current peerConnections
+    for (const peerKey of (peerData as ConnectionData).controlMessage) {
+      if (!peerConnections.current.has(peerKey)) {
+        const conn = peerInstance.current?.connect(peerKey) as DataConnection;
+        connectionControl(conn);
+      }
+    }
+  };
+
+  const handleFetchMediaStatus = (conn: DataConnection) => {
+    console.debug("[room] fetch media status for:", conn.peer);
+    setIsRemoteFetch(conn.peer);
+  };
+
+  const handleUpdateMediaStatus = (peerData: PeerData) => {
+    console.debug(
+      "[room] update local media status to:",
+      peerData.controlMessage
+    );
+    const { mediaSource, mediaAction, mediaTime, mediaSpeed, mediaPlayList } = (
+      peerData as MediaData
+    ).controlMessage;
+    // Local media status
+    setMediaPlayList(mediaPlayList);
+    setMediaSource(mediaSource);
+    setMediaAction(mediaAction);
+    setMediaTime(mediaTime);
+    setMediaSpeed(mediaSpeed);
+    // Remote status
+    setIsRemoteMediaControl(true);
+  };
+
+  const sendSyncPeerInit = (conn: DataConnection) => {
+    conn.send({
+      controlCode: ControlCode.SyncPeerInit,
+      controlMessage: [conn.peer],
+    } as ConnectionData);
+  };
+
+  const sendSyncPeer = (conn: DataConnection, peers: string[]) => {
+    conn.send({
+      controlCode: ControlCode.SyncPeer,
+      controlMessage: peers,
+    } as ConnectionData);
+  };
+
+  const sendFetchMediaStatus = (conn: DataConnection) => {
+    conn.send({
+      controlCode: ControlCode.FetchMediaStatus,
+      controlMessage: [conn.peer],
+    } as ConnectionData);
+  };
+
+  const sendUpdateMediaStatus = (conn: DataConnection, status: RoomStatus) => {
+    conn.send({
+      controlCode: ControlCode.UpdateMediaStatus,
+      controlMessage: {
+        mediaPlayList: status.mediaPlayList,
+        mediaSource: status.mediaSource,
+        mediaAction: status.mediaAction,
+        mediaTime: status.mediaTime,
+        mediaSpeed: status.mediaSpeed,
+      },
+    } as MediaData);
   };
 
   const setRoomStatus = (status: RoomStatus): void => {
@@ -259,30 +285,15 @@ export default function useRoomStatus(): [
         "[room] (setRoomStatus) response remote fetch, send room status to",
         status.isRemoteFetch
       );
-      peerConnections.current.get(status.isRemoteFetch)?.send({
-        controlCode: ControlCode.UpdateMediaStatus,
-        controlMessage: {
-          mediaPlayList: status.mediaPlayList,
-          mediaSource: status.mediaSource,
-          mediaAction: status.mediaAction,
-          mediaTime: status.mediaTime,
-          mediaSpeed: status.mediaSpeed,
-        },
-      } as MediaData);
+      sendUpdateMediaStatus(
+        peerConnections.current.get(status.isRemoteFetch)!,
+        status
+      );
       setIsRemoteFetch(null);
     } else {
       console.debug("[room] (setRoomStatus) send room status to all");
       peerConnections.current.forEach((conn) => {
-        conn.send({
-          controlCode: ControlCode.UpdateMediaStatus,
-          controlMessage: {
-            mediaPlayList: status.mediaPlayList,
-            mediaSource: status.mediaSource,
-            mediaAction: status.mediaAction,
-            mediaTime: status.mediaTime,
-            mediaSpeed: status.mediaSpeed,
-          },
-        } as MediaData);
+        sendUpdateMediaStatus(conn, status);
       });
     }
   };
